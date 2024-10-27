25b3273f28b2fc82c9e00a024d9f7110873c3004
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-10-27 17:01:45 +0100

    First

---
+++ mod.lua
@@ -0,0 +1,21 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "train", "siemens", "vectron", "smartron", "electric", "multi-system", "universal" },
+		minGameVersion = 0,
+	},
+
+  }
+end
+
---
+++ strings.lua
@@ -0,0 +1,137 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Siemens Vectron MS und AC Repaint Pack 1",
+		["mod_desc"] = "This is the repaint package for the Siemens Vectron MS and AC from the ModWerkstatt. There are 89 paint finishes included in this package, SBB paint finishes were created in cooperation and under permission of SBB Cargo International and Railcolor.",
+
+        ["br193_ms_menu"] = "Vectron class 193 MS",
+        ["br193_ac_menu"] = "Vectron class 193 AC",
+        ["br193_fake_ms_menu"] = "Vectron class 193 MS FAKES",
+        ["br193_fake_ac_menu"] = "Vectron class 193 AC FAKES",
+		["br193_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. In 2014 the Vectron MS was presented for the first time with a speed of 200 km/h (also available with 160 km/h) and a continuous output of 6400 kW.",
+
+		["br192_menu"] = "Smartron class 192",
+		["br192_fake_menu"] = "Smartron class 192",
+		["br192_desc"] = "The Smartron is a slimmed down version of the Vectron. The Smartron is only available in blue metallic, the power is 5600 kW and is only approved for 140 km/h, the locomotive only runs in one country at a time. Smartron has the train protection systems PZB/LZB and ETCS for Germany.",
+
+		["br193_ms"] = 	"Vectron as a multi-system locomotive with 4 pantographs",
+		["br193_ac"] = 	"Vectron as a multi-system locomotive with 2 pantographs",
+		["br193_zza"] = "Vectron as an AC locomotive with 2 pantographs and a train destination indicator",
+		["br192"] = 	"Smartron as AC locomotive with 2 pantographs",
+
+		["Ms_br193"] = "Activate Vectron class 193 multi-system?",
+		["option_ms_br193_desc"] = "Should the Vectron class 193 be activated for the multi-system (pantos down, no light, no driver)?",
+		["Ac_br193"] = "Activate Vectron class 193 AC system?",
+		["option_ac_br193_desc"] = "Should the Vectron class 193 be activated for the AC system (pantos down, no light, no driver)?",
+		["dc_br193"] = "Activate Vectron class 193 DC system?",
+		["option_dc_br193_desc"] = "Should the Vectron class 193 be activated for the DC system (Pantos down, no light, no driver)?",
+		["Sm_br193"] = "Activate Smartron class 192?",
+		["option_sm_br193_desc"] = "Should the Smartron class 192 Pantos bottom, no light, no driver) be activated?",
+		["dachinl_br193"] = "Activate Vectron class 193 with DACHINL equipment?",
+		["option_dachinal_br193_desc"] = "Should the Vectron class 193 be activated with DACHINL equipment (pantos down, no lights, no driver)?",
+		["osteuropa_br193"] = "Activate Vectron class 193 with Eastern Europe equipment?",
+		["option_osteuropa_br193_desc"] = "Should the Vectron class 193 be activated with Eastern European equipment (pantos down, no lights, no driver)?",
+		["skandinavien_br193"] = "Activate Smartron Vectron class 193 with Scandinavia equipment?",
+		["option_skandinavien_br193_desc"] = "Should the Vectron class 193 be activated with Scandinavian equipment (pantos down, no lights, no driver)?",
+
+		["br193_ms_sbb13_name"] = 		"Class 193 139 MS SBBCINT Italypiercer (2024)",		
+		["br193_ms_sbb14_name"] = 		"Class 193 45x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb15_name"] = 		"Class 193 45x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb16_name"] = 		"Class 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb17_name"] = 		"Class 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb18_name"] = 		"Class 193 05x MS SBBC PLATZHALTER",
+		["br193_ms_sbb19_name"] = 		"Class 193 05x MS SBBC PLATZHALTER",
+
+		--- AC ---
+		
+		["br193_ac_sbb2_name"] = 		"Class 193 051 AC SBBC",
+
+        -- Fake
+        ["Fake_br193"] = "Active fake engines?",
+        ["option_fake_br193_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
+
+		["br193_ms_sbb13_name_fake"] = 		"Fake - Class 193 139 MS SBBCINT Italypiercer (2024)",		
+		["br193_ms_sbb14_name_fake"] = 		"Fake - Class 193 45x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb15_name_fake"] = 		"Fake - Class 193 45x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb16_name_fake"] = 		"Fake - Class 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb17_name_fake"] = 		"Fake - Class 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb18_name_fake"] = 		"Fake - Class 193 05x MS SBBC PLATZHALTER",
+		["br193_ms_sbb19_name_fake"] = 		"Fake - Class 193 05x MS SBBC PLATZHALTER",
+
+		--- AC ---
+
+		["br193_ac_sbb2_name_fake"] = 		"Fake - Class 193 051 AC SBBC",
+
+
+	},
+	de = {
+
+		["mod_name"] = "Siemens Vectron MS und AC Repaint Paket 1",
+		["mod_desc"] = "Dies ist das Repaintpaket für die Siemens Vectron MS und AC von der ModWerkstatt. In diesem Paket sind weitere 89 Lackierungen enthalten. Lackierungen der SBB entstanden in Kooperation und mit Erlaubnis der SBB Cargo International und Railcolor.",
+
+        ["br193_ms_menu"] = "Vectron BR 193 MS",
+		["br193_ac_menu"] = "Vectron BR 193 AC",
+		["br193_dc_menu"] = "Vectron BR 193 DC",
+        ["br193_fake_ms_menu"] = "Vectron BR 193 MS Fakes",
+        ["br193_fake_ac_menu"] = "Vectron BR 193 AC Fakes",
+        ["br193_fake_dc_menu"] = "Vectron BR 193 DC Fakes",
+		["br193_desc"] = "Der Vectron ist eine moderne Drehstromlokomotive für den internationalen Personen- und Güterverkehr in Europa. 2014 wurde erstmal der Vectron MS vorgestellt mit einer Geschwindigkeit von 200 km/h (auch mit 160 km/h erhältlich) und einer Dauerleistung von 6400 kW.",
+
+		["br192_menu"] = "Smartron BR 192",
+		["br192_fake_menu"] = "Smartron BR 192",
+		["br192_desc"] = "Die Smartron ist eine abgespeckte Version der Vectron. Die Smartron ist nur in blau metallic erhältlich, die Leistung beträgt 5600 kW und ist nur auf 140 km/h zugelassen, die Lok darf auch immer nur in einem Land verkehren. Die Smartron besitzt für Deutschland die Zugsicherungssysteme PZB/LZB und ETCS. ",
+
+		["br193_ms"] = 	"Vectron als Mehrsystem-Lok mit 4 Stromabnehmern",
+		["br193_ac"] = 	"Vectron als Wechselstrom-Lok mit 2 Stromabnehmern",
+		["br193_zza"] = "Vectron als Wechselstrom-Lok mit 2 Stromabnehmern und Zugzielanzeiger",
+		["br192"] = 	"Smartron als Wechselstrom-Lok mit 2 Stromabnehmern",
+
+		["Ms_br193"] = "Vectron BR 193 Mehrsystem aktivieren?",
+		["option_ms_br193_desc"] = "Soll die Vectron BR 193 für das Mehrsystem (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["Ac_br193"] = "Vectron BR 193 Wechselstromsystem aktivieren?",
+		["option_ac_br193_desc"] = "Soll die Vectron BR 193 für das Wechselstromsystem (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["dc_br193"] = "Vectron BR 193 Gleichstromsystem aktivieren?",
+		["option_dc_br193_desc"] = "Soll die Vectron Br 193 für das Gleichstromsystem (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["Sm_br193"] = "Smartron BR 192 aktivieren?",
+		["option_sm_br193_desc"] = "Soll die Smartron BR 192 (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["dachinl_br193"] = "Vectron BR 193 mit DACHINL Ausrüstung aktivieren?",
+		["option_dachinal_br193_desc"] = "Soll die Vectron BR 193 mit DACHINL Ausrüstung (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["osteuropa_br193"] = "Vectron BR 193 mit Osteuropa Ausrüstung aktivieren?",
+		["option_osteuropa_br193_desc"] = "Soll die Vectron BR 193 mit Osteuropa Ausrüstung (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["skandinavien_br193"] = "Smartron Vectron BR 193 mit Skandinavien Ausrüstung aktivieren?",
+		["option_skandinavien_br193_desc"] = "Soll die Vectron BR 193 mit Skandinavien Ausrüstung  (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+
+		
+		["br193_ms_sbb13_name"] = 		"BR 193 139 MS SBBCINT Italienpiercer (2024)",		
+		["br193_ms_sbb14_name"] = 		"BR 193 547 MS SBBCINT Alppiercer 3 (xxxx)",
+		["br193_ms_sbb15_name"] = 		"BR 193 543 MS SBBCINT #BorderlessSwissQuality (xxx)",
+		["br193_ms_sbb16_name"] = 		"BR 193 451 MS SBBCINT 1500. Siemens Vectron (Bavaria München)",
+		["br193_ms_sbb17_name"] = 		"BR 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb18_name"] = 		"BR 193 051 MS SBBC",
+		["br193_ms_sbb19_name"] = 		"BR 193 05x MS SBBC PLATZHALTER",
+
+		--- AC ---
+
+		["br193_ac_sbb2_name"] = 		"BR 193 051 AC SBBC",
+
+        -- Fake
+        ["Fake_br193"] = "Fake-Loks aktivieren?",
+        ["option_fake_br193_desc"] = "Die Fake-Loks (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+
+		["br193_ms_sbb13_name_fake"] = 		"Fake - BR 193 139 MS SBBCINT Italienpiercer (2024)",		
+		["br193_ms_sbb14_name_fake"] = 		"Fake - BR 193 45x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb15_name_fake"] = 		"Fake - BR 193 45x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb16_name_fake"] = 		"Fake - BR 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb17_name_fake"] = 		"Fake - BR 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb18_name_fake"] = 		"Fake - BR 193 05x MS SBBC PLATZHALTER",
+		["br193_ms_sbb19_name_fake"] = 		"Fake - BR 193 05x MS SBBC PLATZHALTER",
+
+		--- AC ---
+		
+		["br193_ac_sbb2_name_fake"] = 		"Fake - BR 193 051 AC SBBC",
+
+
+	},
+}
+end
