c4f4bc9a51a629cb5b8ec967bcde685471f075ad
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-12-09 20:00:11 +0100

    Mod aufgeräumt + Neu 193 659(Güterbahnen)

--- strings.lua
+++ strings.lua
@@ -5,36 +5,6 @@ return {
 		["mod_name"] = "Siemens Vectron MS und AC Repaint Pack 2",
 		["mod_desc"] = "This is the repaint package for the Siemens Vectron MS and AC from the ModWerkstatt. There are 89 paint finishes included in this package, SBB paint finishes were created in cooperation and under permission of SBB Cargo International and Railcolor.",
 
-        ["br193_ms_menu"] = "Vectron class 193 MS",
-        ["br193_ac_menu"] = "Vectron class 193 AC",
-        ["br193_fake_ms_menu"] = "Vectron class 193 MS FAKES",
-        ["br193_fake_ac_menu"] = "Vectron class 193 AC FAKES",
-		["br193_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. In 2014 the Vectron MS was presented for the first time with a speed of 200 km/h (also available with 160 km/h) and a continuous output of 6400 kW.",
-
-		["br192_menu"] = "Smartron class 192",
-		["br192_fake_menu"] = "Smartron class 192",
-		["br192_desc"] = "The Smartron is a slimmed down version of the Vectron. The Smartron is only available in blue metallic, the power is 5600 kW and is only approved for 140 km/h, the locomotive only runs in one country at a time. Smartron has the train protection systems PZB/LZB and ETCS for Germany.",
-
-		["br193_ms"] = 	"Vectron as a multi-system locomotive with 4 pantographs",
-		["br193_ac"] = 	"Vectron as a multi-system locomotive with 2 pantographs",
-		["br193_zza"] = "Vectron as an AC locomotive with 2 pantographs and a train destination indicator",
-		["br192"] = 	"Smartron as AC locomotive with 2 pantographs",
-
-		["Ms_br193"] = "Activate Vectron class 193 multi-system?",
-		["option_ms_br193_desc"] = "Should the Vectron class 193 be activated for the multi-system (pantos down, no light, no driver)?",
-		["Ac_br193"] = "Activate Vectron class 193 AC system?",
-		["option_ac_br193_desc"] = "Should the Vectron class 193 be activated for the AC system (pantos down, no light, no driver)?",
-		["dc_br193"] = "Activate Vectron class 193 DC system?",
-		["option_dc_br193_desc"] = "Should the Vectron class 193 be activated for the DC system (Pantos down, no light, no driver)?",
-		["Sm_br193"] = "Activate Smartron class 192?",
-		["option_sm_br193_desc"] = "Should the Smartron class 192 Pantos bottom, no light, no driver) be activated?",
-		["dachinl_br193"] = "Activate Vectron class 193 with DACHINL equipment?",
-		["option_dachinal_br193_desc"] = "Should the Vectron class 193 be activated with DACHINL equipment (pantos down, no lights, no driver)?",
-		["osteuropa_br193"] = "Activate Vectron class 193 with Eastern Europe equipment?",
-		["option_osteuropa_br193_desc"] = "Should the Vectron class 193 be activated with Eastern European equipment (pantos down, no lights, no driver)?",
-		["skandinavien_br193"] = "Activate Smartron Vectron class 193 with Scandinavia equipment?",
-		["option_skandinavien_br193_desc"] = "Should the Vectron class 193 be activated with Scandinavian equipment (pantos down, no lights, no driver)?",
-		
 		["br193_ms_sbb_name"] = 		"Class 193 107 MS SBBCINT Silverpiercer (Lake Constance)",
 		["br193_ms_sbb2_name"] = 		"Class 193 258 MS SBBCINT/ELL",
 		["br193_ms_sbb3_name"] = 		"Class 193 658 MS SBBCINT Shadowpiercer (Lake Lucerne)",
@@ -48,12 +18,10 @@ return {
 		["br193_ms_sbb11_name"] = 		"Class 193 110 MS SBBCINT Goldpiercer (Zuckersee)",
 		["br193_ms_sbb12_name"] = 		"Class 193 492 MS SBBCINT Nightpiercer (Rhine)",
 		["br193_ms_sbb13_name"] = 		"Class 193 139 MS SBBCINT Italypiercer (2024)",		
-		["br193_ms_sbb14_name"] = 		"Class 193 45x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb15_name"] = 		"Class 193 45x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb16_name"] = 		"Class 193 54x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb17_name"] = 		"Class 193 54x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb18_name"] = 		"Class 193 05x MS SBBC PLATZHALTER",
-		["br193_ms_sbb19_name"] = 		"Class 193 05x MS SBBC PLATZHALTER",
+		["br193_ms_sbb14_name"] = 		"Class 193 544 MS SBBCINT Alppiercer 3 (2024)",
+		["br193_ms_sbb15_name"] = 		"Class 193 543 MS SBBCINT #BorderlessSwissQuality (2024)",
+		["br193_ms_sbb16_name"] = 		"Class 193 451 MS SBBCINT 1500. Siemens Vectron (2024)",
+		["br193_ms_sbb17_name"] = 		"Class 193 659 MS SBBCINT Die Güterbahnen (2022)",
 
 		--- AC ---
 		
@@ -61,15 +29,9 @@ return {
 		["br193_ac_sbb2_name"] = 		"Class 193 051 AC SBBC",
 
         -- Vorspann
-        ["Vorspann_br193"] = "Active lead engines?",
-        ["option_vorspann_br193_desc"] = "Active the lead engines (with front panto raised)?",
-
 		["br193_ac_sbb_name_vorspann"] = 		"Lead - Class 193 210 AC SBBCINT/ELL",
 
         -- Fake
-        ["Fake_br193"] = "Active fake engines?",
-        ["option_fake_br193_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
-
 		["br193_ms_sbb_name_fake"] = 		"Fake - Class 193 107 MS SBBCINT Silverpiercer (Lake Constance)",
 		["br193_ms_sbb2_name_fake"] = 		"Fake - Class 193 258 MS SBBCINT/ELL",
 		["br193_ms_sbb3_name_fake"] = 		"Fake - Class 193 658 MS SBBCINT Shadowpiercer (Lake Lucerne)",
@@ -83,17 +45,14 @@ return {
 		["br193_ms_sbb11_name_fake"] = 		"Fake - Class 193 110 MS SBBCINT Goldpiercer (Zuckersee)",
 		["br193_ms_sbb12_name_fake"] = 		"Fake - Class 193 492 MS SBBCINT Nightpiercer (Rhine)",
 		["br193_ms_sbb13_name_fake"] = 		"Fake - Class 193 139 MS SBBCINT Italypiercer (2024)",		
-		["br193_ms_sbb14_name_fake"] = 		"Fake - Class 193 45x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb15_name_fake"] = 		"Fake - Class 193 45x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb16_name_fake"] = 		"Fake - Class 193 54x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb17_name_fake"] = 		"Fake - Class 193 54x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb18_name_fake"] = 		"Fake - Class 193 05x MS SBBC PLATZHALTER",
-		["br193_ms_sbb19_name_fake"] = 		"Fake - Class 193 05x MS SBBC PLATZHALTER",
+		["br193_ms_sbb14_name_fake"] = 		"Fake - BR 193 544 MS SBBCINT Alppiercer 3 (2024)",
+		["br193_ms_sbb15_name_fake"] = 		"Fake - BR 193 543 MS SBBCINT #BorderlessSwissQuality (2024)",
+		["br193_ms_sbb16_name_fake"] = 		"Fake - BR 193 451 MS SBBCINT 1500. Siemens Vectron (2024)",
+		["br193_ms_sbb17_name_fake"] = 		"Fake - BR 193 659 MS SBBCINT Die Güterbahnen (2022)",
 
 		--- AC ---
 
 		["br193_ac_sbb_name_fake"] = 		"Fake - Class 193 210 AC SBBCINT/ELL",
-		["br193_ac_sbb2_name_fake"] = 		"Fake - Class 193 051 AC SBBC",
 
 
 	},
@@ -118,22 +77,16 @@ return {
 		["br193_ms_sbb14_name"] = 		"BR 193 544 MS SBBCINT Alppiercer 3 (2024)",
 		["br193_ms_sbb15_name"] = 		"BR 193 543 MS SBBCINT #BorderlessSwissQuality (2024)",
 		["br193_ms_sbb16_name"] = 		"BR 193 451 MS SBBCINT 1500. Siemens Vectron (2024)",
-		["br193_ms_sbb17_name"] = 		"BR 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb17_name"] = 		"BR 193 659 MS SBBCINT Die Güterbahnen (2022)",
 
 		--- AC ---
 
 		["br193_ac_sbb_name"] = 		"BR 193 210 AC SBBCINT/ELL",
 		
 		-- Vorspann
-        ["Vorspann_br193"] = "Vorspann-Loks aktivieren?",
-        ["option_vorspann_br193_desc"] = "Die Vorspann-Loks (mit vorderem Panto gehoben) aktiviert werden?",
-
 		["br193_ac_sbb_name_vorspann"] = 		"Vorspann - BR 193 210 AC SBBCINT/ELL",
 
         -- Fake
-        ["Fake_br193"] = "Fake-Loks aktivieren?",
-        ["option_fake_br193_desc"] = "Die Fake-Loks (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		
 		["br193_ms_sbb_name_fake"] = 		"Fake - BR 193 107 MS SBBCINT Silverpiercer (Bodensee)",
 		["br193_ms_sbb2_name_fake"] = 		"Fake - BR 193 258 MS SBBCINT/ELL",
 		["br193_ms_sbb3_name_fake"] = 		"Fake - BR 193 658 MS SBBCINT Shadowpiercer (Vierwaldstättersee)",
@@ -147,10 +100,10 @@ return {
 		["br193_ms_sbb11_name_fake"] = 		"Fake - BR 193 110 MS SBBCINT Goldpiercer (Zuckersee)",
 		["br193_ms_sbb12_name_fake"] = 		"Fake - BR 193 492 MS SBBCINT Nightpiercer (Rhein)",
 		["br193_ms_sbb13_name_fake"] = 		"Fake - BR 193 139 MS SBBCINT Italienpiercer (2024)",		
-		["br193_ms_sbb14_name_fake"] = 		"Fake - BR 193 45x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb15_name_fake"] = 		"Fake - BR 193 45x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb16_name_fake"] = 		"Fake - BR 193 54x MS SBBCINT PLATZHALTER",
-		["br193_ms_sbb17_name_fake"] = 		"Fake - BR 193 54x MS SBBCINT PLATZHALTER",
+		["br193_ms_sbb14_name_fake"] = 		"Fake - BR 193 544 MS SBBCINT Alppiercer 3 (2024)",
+		["br193_ms_sbb15_name_fake"] = 		"Fake - BR 193 543 MS SBBCINT #BorderlessSwissQuality (2024)",
+		["br193_ms_sbb16_name_fake"] = 		"Fake - BR 193 451 MS SBBCINT 1500. Siemens Vectron (2024)",
+		["br193_ms_sbb17_name_fake"] = 		"Fake - BR 193 659 MS SBBCINT Die Güterbahnen (2022)",
 
 		--- AC ---
 		
